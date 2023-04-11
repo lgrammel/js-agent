9e2fe9b8a7717273bedf6a0e3419b6dfa2cb9928
Lars Grammel <lars.grammel@gmail.com>
2023-04-11 10:33:29 +0200

    Add JSDoc comments.

--- packages/agent/src/action/format/ActionFormat.ts
+++ packages/agent/src/action/format/ActionFormat.ts
@@ -1,7 +1,29 @@
 import { ActionParameters } from "../ActionParameters";
 
+/**
+ * Format for parsing/formatting actions from text.
+ *
+ * This is used to instruct the LLM how to execute actions and to parse the actions that the LLM wants to execute.
+ */
 export interface ActionFormat {
+  /**
+   * A description of the action format.
+   */
   description: string | undefined;
+
+  /**
+   * Formats the given action parameters into a string.
+   *
+   * @param parameters - The action parameters to format.
+   * @returns The formatted string.
+   */
   format(parameters: ActionParameters): string;
+
+  /**
+   * Parses the given text into action parameters.
+   *
+   * @param text - The text to parse.
+   * @returns The parsed action parameters.
+   */
   parse(text: string): ActionParameters;
 }
--- packages/agent/src/action/format/JsonActionFormat.ts
+++ packages/agent/src/action/format/JsonActionFormat.ts
@@ -2,13 +2,31 @@ import { ActionParameters, actionParametersSchema } from "../ActionParameters";
 import { ActionFormat } from "./ActionFormat";
 import SecureJSON from "secure-json-parse";
 
+/**
+ * Class for handling JSON action formats.
+ */
 export class JsonActionFormat implements ActionFormat {
+  /**
+   * A description of the JSON action format.
+   */
   description = "JSON";
 
+  /**
+   * Formats the given action parameters into a JSON string.
+   *
+   * @param parameters - The action parameters to format.
+   * @returns The formatted JSON string.
+   */
   format(parameters: ActionParameters): string {
     return JSON.stringify(parameters, null, 2);
   }
 
+  /**
+   * Parses the given text into action parameters, handling JSON objects and free text.
+   *
+   * @param text - The text to parse.
+   * @returns The parsed action parameters.
+   */
   parse(text: string): ActionParameters {
     if (!text.trim().endsWith("}")) {
       return { _freeText: text };
