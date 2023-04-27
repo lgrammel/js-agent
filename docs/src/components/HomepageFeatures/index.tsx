eb01d7bf2b3bb5849c3f0ea4a0d08cb818202d84
Lars Grammel <lars.grammel@gmail.com>
2023-04-27 20:19:20 +0200

    Add basic feature list.

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -4,49 +4,41 @@ import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  // Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  // {
-  //   title: "Easy to Use",
-  //   Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-  //   description: (
-  //     <>
-  //       Docusaurus was designed from the ground up to be easily installed and
-  //       used to get your website up and running quickly.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: "Focus on What Matters",
-  //   Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: "Powered by React",
-  //   Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
+  {
+    title: "Create agents quickly",
+    description: (
+      <>
+        JS Agent provides many concepts, pre-defined prompts, and tools to help
+        you create agents quickly.
+      </>
+    ),
+  },
+  {
+    title: "Run agents in a server",
+    description: (
+      <>
+        JS Agent contains a HTTP server that host multiple agents. Agent runs
+        can be started, stopped, and observed via HTTP API.
+      </>
+    ),
+  },
+  {
+    title: "Load data",
+    description: <>Loaders for reading PDFs and websites</>,
+  },
 ];
 
-function Feature({ title, Svg, description }: FeatureItem) {
+function Feature({ title, description }: FeatureItem) {
   return (
     <div className={clsx("col col--4")}>
-      <div className="text--center">
+      {/* <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
-      </div>
+      </div> */}
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
