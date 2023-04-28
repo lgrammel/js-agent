835604a385687bf24d322f2bf875aa22a80b567e
Lars Grammel <lars.grammel@gmail.com>
2023-04-28 14:07:59 +0200

    Remove dead code.

--- packages/agent/src/memory/Embedding.ts
+++
@@ -1,1 +0,0 @@
-export type Embedding = number[];
--- packages/agent/src/memory/Memory.ts
+++
@@ -1,6 +0,0 @@
-import { Embedding } from "./Embedding";
-
-export interface Memory<T> {
-  retrieve({}: { embedding: Embedding; maxResults: number }): Promise<Array<T>>;
-  store({}: { embedding: Embedding; item: T }): Promise<void>;
-}
