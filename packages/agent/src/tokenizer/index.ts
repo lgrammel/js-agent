c01eed3318a10738ec17659ed9292ed5f5a20819
Lars Grammel <lars.grammel@gmail.com>
2023-04-29 17:31:51 +0200

    Add docs.

--- packages/agent/src/tokenizer/index.ts
+++ packages/agent/src/tokenizer/index.ts
@@ -1,2 +1,6 @@
 export * from "./Tokenizer";
+
+/**
+ * Tokenizer for OpenAI models (using [`@bqbd/tiktoken`](https://github.com/dqbd/tiktoken/tree/main/js)).
+ */
 export * as openai from "./openai.js";
